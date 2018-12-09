dfa86b70f40103accccce9c2417f997c24746622
KevHg <kevhung17@gmail.com>
2018-12-09 18:31:41 +0800

    Add basic calculation functions

---
+++ src/CalcButton.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CalcButton extends JButton {
+    public CalcButton(String text) {
+        super(text);
+        this.setFont(new Font("Arial", Font.PLAIN, 20));
+    }
+
+}
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,52 +1,152 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class Calculator {
     private JFrame frame;
     private JPanel buttonPanel;
     private JTextArea textScreen;
+    private CalcButton[] numbers = new CalcButton[10];
+    private CalcButton[] operations = new CalcButton[4];
+    private CalcButton[] mod = new CalcButton[3];
+    private CalcButton[] misc = new CalcButton[3];
+    private double prev, val;
+    private String op;
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.initialize();
     }
 
-    private void initialize(){
+    private void initialize() {
+        prev = 0;
+        val = 0;
+        op = null;
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        textScreen = new JTextArea(1,1);
-        textScreen.setPreferredSize(new Dimension(300,200));
+        textScreen = new JTextArea(1, 1);
+        textScreen.setPreferredSize(new Dimension(300, 100));
         textScreen.setLineWrap(true);
         textScreen.setEditable(false);
+        textScreen.setFont(new Font("Arial", Font.PLAIN, 30));
 
         buttonPanel = new JPanel();
-        buttonPanel.setPreferredSize(new Dimension(300,400));
-        buttonPanel.setLayout(new GridLayout(4,3));
+        buttonPanel.setPreferredSize(new Dimension(300, 400));
+        buttonPanel.setLayout(new GridLayout(5, 4));
 
-        JButton button0 = new JButton("0");
-        JButton button1 = new JButton("1");
-        JButton button2 = new JButton("2");
-        JButton button3 = new JButton("3");
-        JButton button4 = new JButton("4");
-        JButton button5 = new JButton("5");
-        JButton button6 = new JButton("6");
-        JButton button7 = new JButton("7");
-        JButton button8 = new JButton("8");
-        JButton button9 = new JButton("9");
-        JButton buttonPlusMin = new JButton("+-");
-        JButton buttonDot = new JButton(".");
-
-        buttonPanel.add(button7); buttonPanel.add(button8); buttonPanel.add(button9);
-        buttonPanel.add(button4); buttonPanel.add(button5); buttonPanel.add(button6);
-        buttonPanel.add(button1); buttonPanel.add(button2); buttonPanel.add(button3);
-        buttonPanel.add(buttonPlusMin); buttonPanel.add(button0); buttonPanel.add(buttonDot);
+        createButtons();
+        setActionListeners();
+        setButtonPanel();
 
         frame.getContentPane().add(textScreen, BorderLayout.NORTH);
         frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
 
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void setActionListeners() {
+        for (JButton button : numbers) {
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    val = val * 10 + Double.valueOf(button.getText());
+                    if (val == (long) val)
+                        textScreen.setText(String.format("%d", (long) val));
+                    else
+                        textScreen.setText(String.format("%f", val));
+                }
+            });
+        }
+
+        for (JButton button : operations) {
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (op == null)
+                        prev = evaluate(prev, val, "+");
+                    else
+                        prev = evaluate(prev, val, op);
+                    val = 0;
+                    op = button.getText();
+                }
+            });
+        }
+
+        //Equal Button
+        mod[2].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (op == null)
+                    prev = val;
+                else
+                    prev = evaluate(prev, val, op);
+                val = 0;
+                op = null;
+                if (prev == (long) prev)
+                    textScreen.setText(String.format("%d", (long) prev));
+                else
+                    textScreen.setText(String.format("%f", prev));
+            }
+        });
+    }
+
+    private double evaluate(double prev, double val, String op) {
+        if (op.equals("+")) {
+            return prev + val;
+        } else if (op.equals("-")) {
+            return prev - val;
+        } else if (op.equals("\u00D7")) {
+            return prev * val;
+        } else {
+            return prev / val;
+        }
+    }
+
+    private void createButtons() {
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = new CalcButton(Integer.toString(i));
+
+        operations[0] = new CalcButton("+");
+        operations[1] = new CalcButton("-");
+        operations[2] = new CalcButton("\u00D7");
+        operations[3] = new CalcButton("\u00F7");
+
+        mod[0] = new CalcButton("\u00B1");
+        mod[1] = new CalcButton(".");
+        mod[2] = new CalcButton("=");
+
+        misc[0] = new CalcButton("C");
+        misc[1] = new CalcButton("CE");
+        misc[2] = new CalcButton("\u2190");
+    }
+
+    private void setButtonPanel() {
+        buttonPanel.add(misc[0]);
+        buttonPanel.add(misc[1]);
+        buttonPanel.add(misc[2]);
+        buttonPanel.add(operations[3]);
+
+        for (int i = 7; i <= 9; i++)
+            buttonPanel.add(numbers[i]);
+        buttonPanel.add(operations[2]);
+
+        for (int i = 4; i <= 6; i++)
+            buttonPanel.add(numbers[i]);
+        buttonPanel.add(operations[1]);
+
+        for (int i = 1; i <= 3; i++)
+            buttonPanel.add(numbers[i]);
+        buttonPanel.add(operations[0]);
+
+        buttonPanel.add(mod[0]);
+        buttonPanel.add(numbers[0]);
+        buttonPanel.add(mod[1]);
+        buttonPanel.add(mod[2]);
     }
 
 }
